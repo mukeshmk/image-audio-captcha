ec333f26d467370aad5ee8f6df8ef2d98c9130b2
Mukesh A <amukesh.mk@gmail.com>
2019-10-13 03:19:52 +0100

    testing with splitting audio files based on silences

--- audio-captcha.py
+++ audio-captcha.py
@@ -5,11 +5,14 @@ import numpy as np
 from tqdm import tqdm
 from pydub import AudioSegment
 import matplotlib.pyplot as plt
+from pydub.silence import split_on_silence
 from sklearn.model_selection import train_test_split
 
 FILE_FORMAT = '.mp3'
-DATA_PATH = "D:\\code\\github-clones\\image-audio-captcha\\test\\"
-NPY_PATH = "D:\\code\\github-clones\\image-audio-captcha\\npy\\"
+PWD = os.getcwd()
+DATA_PATH = PWD + '\\test\\'
+NPY_PATH = PWD + '\\npy\\'
+IMG_PATH = DATA_PATH + 'img\\'
 
 
 def to_categorical(y, num_classes=None, dtype='float32'):
@@ -61,7 +64,26 @@ def save_data_to_array(path=DATA_PATH, max_len=11, n_mfcc=20):
         np.save(NPY_PATH + label.strip(FILE_FORMAT) + '.npy', mfcc_vectors)
 
 
-def asdf():
+def get_train_test(split_ratio=0.6, random_state=42):
+    # Get available labels
+    labels, indices, _ = get_labels(DATA_PATH)
+
+    # Getting first arrays
+    X = np.load(NPY_PATH + labels[0].strip(FILE_FORMAT) + '.npy')
+    y = np.zeros(X.shape[0])
+
+    # Append all of the dataset into one single array, same goes for y
+    for i, label in enumerate(labels[1:]):
+        x = np.load(NPY_PATH + label.strip(FILE_FORMAT) + '.npy')
+        X = np.vstack((X, x))
+        y = np.append(y, np.full(x.shape[0], fill_value=(i + 1)))
+
+    assert X.shape[0] == len(y)
+
+    return train_test_split(X, y, test_size=(1 - split_ratio), random_state=random_state, shuffle=True)
+
+
+def convert_audio_to_spectrogram():
     mp3_audio_path = DATA_PATH + '2I1UP8QY' + FILE_FORMAT
     sound = AudioSegment.from_mp3(mp3_audio_path)
     sound_samples = sound.get_array_of_samples()  # Extract signal samples
@@ -82,30 +104,21 @@ def asdf():
     plt.close(fg2)
 
 
-def get_train_test(split_ratio=0.6, random_state=42):
-    # Get available labels
-    labels, indices, _ = get_labels(DATA_PATH)
+def split_audio_silence():
+    sound = AudioSegment.from_mp3(PWD + '\\00C643SS.wav')
+    print(len(sound))
+    print(sound.dBFS)
+    clips = split_on_silence(sound, min_silence_len=15, silence_thresh=-23)
+    print(len(clips))
+    c = 0
+    for i in clips:
+        i.export(str(c) + '.wav')
+        c += 1
 
-    # Getting first arrays
-    X = np.load(NPY_PATH + labels[0].strip(FILE_FORMAT) + '.npy')
-    y = np.zeros(X.shape[0])
-
-    # Append all of the dataset into one single array, same goes for y
-    for i, label in enumerate(labels[1:]):
-        x = np.load(NPY_PATH + label.strip(FILE_FORMAT) + '.npy')
-        X = np.vstack((X, x))
-        y = np.append(y, np.full(x.shape[0], fill_value=(i + 1)))
-
-    assert X.shape[0] == len(y)
-
-    return train_test_split(X, y, test_size=(1 - split_ratio), random_state=random_state, shuffle=True)
-
-
+'''
 save_data_to_array(DATA_PATH)
 
 X_train, X_test, y_train, y_test = get_train_test()
-
-print(X_train)
 
 max_len = 11
 buckets = 20
@@ -113,6 +126,5 @@ channels = 1
 
 X_train = X_train.reshape(X_train.shape[0], buckets, max_len, channels)
 X_test = X_test.reshape(X_test.shape[0], buckets, max_len, channels)
-
-print(X_train)
-# asdf()
+'''
+split_on_silence()
