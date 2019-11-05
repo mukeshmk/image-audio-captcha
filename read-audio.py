c95de2971d338aec5d7db492edb41229f504f5fd
Mukesh A <amukesh.mk@gmail.com>
2019-11-05 23:42:25 +0000

    parallelising convert audio to image code and changed the image generation method

--- read-audio.py
+++ read-audio.py
@@ -8,34 +8,40 @@ import matplotlib
 import librosa
 import librosa.display
 from pydub import AudioSegment
+from multiprocessing import Pool
 
 matplotlib.rcParams['figure.figsize'] = [15, 10]
 
 
-def convert_audio_image(pwd, input_dir, output_dir):
-    file_names = os.listdir(input_dir)
-    for file in file_names:
+def convert_single_audio_to_image(pwd, input_dir, output_dir, file):
+    try:
+        print('Processing: {0}'.format(file))
         mp3_audio_path = pwd + '\\' + input_dir + '\\' + file
-        sound = AudioSegment.from_mp3(mp3_audio_path)
-        wav_audio_path = pwd + '/tempDir/' + file.strip('.mp3') + '.wav'
-        sound.export(wav_audio_path, format="wav")
+        # load and process audio
+        audio, sr = librosa.load(mp3_audio_path)
+        # audio = lowpass(audio, cutoff=3000, sample_freq=sr)
+        spec = librosa.stft(np.asfortranarray(audio))
+        spec_db = librosa.amplitude_to_db(np.abs(spec))
 
-        plt.interactive(False)
-        clip, sample_rate = librosa.load(wav_audio_path, sr=None)
-        fig = plt.figure(figsize=[0.415, 0.210])
-        ax = fig.add_subplot(111)
-        ax.axes.get_xaxis().set_visible(False)
-        ax.axes.get_yaxis().set_visible(False)
-        ax.set_frame_on(False)
-        S = librosa.feature.melspectrogram(y=clip, sr=sample_rate)
-        librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
-        filename = os.path.join(output_dir, file + '.jpg')
-        plt.savefig(filename, dpi=400, bbox_inches='tight', pad_inches=0)
+        # generate plot of size 128x64
+        fig = plt.figure(figsize=(1.28, 0.64))
+        plt.box(False)
+        plt.subplots_adjust(left=0, right=1, bottom=0, wspace=0, hspace=0)
+        librosa.display.specshow(spec_db, sr=sr, cmap='gray_r', x_axis='time', y_axis='log')
+        fig.savefig(output_dir + '/' + file.strip('.mp3'), bbox_inches=None, pad_inches=0)
         plt.close()
-        fig.clf()
-        plt.close(fig)
-        plt.close('all')
-        del clip, sample_rate, fig, ax, S
+    except Exception as e:
+        print('processing {0}: {1}'.format(file, e))
+
+
+def convert_audio_image(pwd, input_dir, output_dir, n):
+    file_names = os.listdir(input_dir)
+    process_args = []
+    for file in file_names:
+        process_args.append((pwd, input_dir, output_dir, file))
+
+    with Pool(n) as p:
+        p.starmap(convert_single_audio_to_image, process_args)
 
 
 def main():
@@ -43,6 +49,7 @@ def main():
     parser.add_argument('--input-dir', help='input dir of captcha audio set', type=str)
     parser.add_argument('--output-dir', help='Where to store the generated captchas', type=str)
     parser.add_argument('--symbols', help='File with the symbols to use in captchas', type=str)
+    parser.add_argument('--n', help='no of parallel threads to start', type=int, default=6)
     args = parser.parse_args()
 
     if args.input_dir is None:
@@ -56,6 +63,10 @@ def main():
     if args.symbols is None:
         print("Please specify the captcha symbols file")
         exit(1)
+
+    if args.n is None:
+        print("Setting the number of threads to 6")
+        args.n = 6
 
     symbols_file = open(args.symbols, 'r')
     captcha_symbols = symbols_file.readline().strip()
@@ -71,7 +82,9 @@ def main():
     if not os.path.exists(args.input_dir + tempDir):
         print("Creating temp directory " + tempDir)
         os.makedirs(args.input_dir + tempDir)
-    convert_audio_image(os.getcwd(), args.input_dir, args.output_dir)
+
+    convert_audio_image(os.getcwd(), args.input_dir, args.output_dir, args.n)
+
     if os.path.exists(args.input_dir + tempDir):
         print("Deleting temp directory " + tempDir)
         os.rmdir(args.input_dir + tempDir)
